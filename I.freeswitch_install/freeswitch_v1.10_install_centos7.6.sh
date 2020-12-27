8c8fae37236e1f75a8908b8ec588bbd7ae635e59
zhoudd <dong_abc@126.com>
2020-12-27 01:33:19 -0800

    Update I.freeswitch_install

---
+++ I.freeswitch_install/freeswitch_v1.10_install_centos7.6.sh
@@ -0,0 +1,5 @@
+yum install -y https://files.freeswitch.org/repo/yum/centos-release/freeswitch-release-repo-0-1.noarch.rpm epel-release
+yum install -y freeswitch-config-vanilla freeswitch-lang-* freeswitch-sounds-*
+systemctl enable freeswitc
+
+https://freeswitch.org/confluence/display/FREESWITCH/CentOS+7+and+RHEL+7
