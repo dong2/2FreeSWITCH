05b41c2accad9c263f82c2b7274244a128c98190
zhoudd <zhoudd@zhoudd-ubuntu>
2020-12-28 16:59:08 +0800

    Update VIV.freeswitch_webrtc

--- I.freeswitch_install/freeswitch_v1.10_install_centos7.6.sh
+++ I.freeswitch_install/freeswitch_v1.10_install_centos7.6.sh
@@ -43,6 +43,70 @@ make -j install
 make -j cd-sounds-install
 make -j cd-moh-install
 
+
+# 补上mod_av模块
+wget https://www.nasm.us/pub/nasm/releasebuilds/2.14.02/nasm-2.14.02.tar.xz
+tar xf nasm-2.14.02.tar.xz
+cd nasm-2.14.02
+./configure
+make
+make install
+
+git clone https://gitee.com/dong2/x264.git
+# the output directory may be difference depending on the version and date
+cd x264
+./configure --enable-shared --enable-pic
+make
+make install
+
+wget https://libav.org/releases/libav-12.3.tar.xz
+tar xf libav-12.3.tar.xz
+cd libav-12.3
+# 进入 libav 源码目录下, 将 libavcodec/libx264.c 文件里面的 "x264_bit_depth" 全部替换为 "X264_BIT_DEPTH"，否则编译会报错。
+./configure --enable-shared --enable-libx264 --enable-gpl
+make
+make install
+ln -sf /usr/local/lib/pkgconfig/libavcodec.pc  /usr/lib64/pkgconfig/libavcodec.pc
+ln -sf /usr/local/lib/pkgconfig/libavdevice.pc  /usr/lib64/pkgconfig/libavdevice.pc
+ln -sf /usr/local/lib/pkgconfig/libavfilter.pc  /usr/lib64/pkgconfig/libavfilter.pc
+ln -sf /usr/local/lib/pkgconfig/libavformat.pc  /usr/lib64/pkgconfig/libavformat.pc
+ln -sf /usr/local/lib/pkgconfig/libavresample.pc  /usr/lib64/pkgconfig/libavresample.pc
+ln -sf /usr/local/lib/pkgconfig/libavutil.pc  /usr/lib64/pkgconfig/libavutil.pc
+ln -sf /usr/local/lib/pkgconfig/libswscale.pc  /usr/lib64/pkgconfig/libswscale.pc
+
+ldconfig
+# 然后像上面一样
+./configure
+export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig
+make
+make -j install
+make -j cd-sounds-install
+make -j cd-moh-install
+
+# 补上mod_signalwire，大多数情况不需要mod_signalwire，不要轻易添加mod_signalwire.
+
+yum install libatomic -y
+
+wget https://github.com/Kitware/CMake/releases/download/v3.15.2/cmake-3.15.2.tar.gz
+tar xf cmake-3.15.2.tar.gz
+cd cmake-3.15.2
+./bootstrap 
+gmake
+make install
+
+git clone https://github.com/signalwire/libks.git
+cd libks
+cmake .
+make
+make install
+
+git clone https://github.com/signalwire/signalwire-c.git
+cd signalwire-c
+cmake .
+make
+make install
+
 # reference
 https://freeswitch.org/confluence/display/FREESWITCH/CentOS+7+and+RHEL+7
 https://blog.csdn.net/jiaojian8063868/article/details/110929209
+https://zhuanlan.zhihu.com/p/153395654
